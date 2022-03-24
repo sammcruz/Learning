cfed7d678ee5709de3d8f790089410d3be488051
sammcruz <samantha@poli.ufrj.br>
2022-03-24 20:25:57 -0300

    Inicio da aula 4 sobre estatística

---
+++ Curso_Paula_Mar2022/Anotacoes_Aula4.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 24 18:13:07 2022
+
+@author: samantha
+"""
+
+import pandas as pd
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+import statistics
+import scipy.stats
+
+
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Learning codes
 
-This repo contains codes used in all programming examples and assignments for Udemy, Coursera (and others) courses.
+This repo contains scripts used in courses and learning projects at Udemy, Coursera (and others).
 
 
 ..
